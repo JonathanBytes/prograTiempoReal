f2732b915c8e4e580cda9556fb9ae658740722f3
Jonathan ArchDell <jonathancardonaramirez@gmail.com>
2022-08-24 11:41:11 -0500

    Lee coeficientes del polinomio del tamaño deseado y los imprime en orden poly[0] = coeficiente de x^0

--- taller2/terceroTaller2/Program.cs
+++ taller2/terceroTaller2/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿int orden;
+System.Console.Write("Ingrese el orden del polinomio: ");
+orden = Convert.ToInt32(Console.ReadLine());
+
+double[] poly = new double[orden + 1];
+
+for (int i = 0; i <= orden; i++)
+{
+    System.Console.Write("Ingrese la constante que acompaña x^{0}: ", orden - i);
+    poly[orden - i] = Convert.ToDouble(Console.ReadLine());
+}
+
+foreach (double j in poly)
+{
+    System.Console.WriteLine(j);
+}
+
+System.Console.WriteLine("First position {0}", poly[0]);
+System.Console.WriteLine("Third position {0}", poly[3]);
