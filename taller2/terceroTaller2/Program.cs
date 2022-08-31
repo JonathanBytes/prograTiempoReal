a362d24097b65900ff0449886043788433b7c4f0
Jonathan ArchDell <jonathancardonaramirez@gmail.com>
2022-08-31 09:23:49 -0500

    Ruffini madafaka

--- taller2/terceroTaller2/Program.cs
+++ taller2/terceroTaller2/Program.cs
@@ -1,6 +1,11 @@
-﻿int orden;
-System.Console.Write("Ingrese el orden del polinomio: ");
+﻿int orden, equis;
+bool continuar = true;
+
+System.Console.Clear();
+
+System.Console.Write("\nIngrese el orden del polinomio: ");
 orden = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
 
 double[] poly = new double[orden + 1];
 
@@ -17,3 +22,36 @@ foreach (double j in poly)
 
 System.Console.WriteLine("First position {0}", poly[0]);
 System.Console.WriteLine("Third position {0}", poly[3]);
+
+System.Console.WriteLine("\nIngrese el valor de x: ");
+equis = Convert.ToInt32(Console.ReadLine());
+
+System.Console.ReadKey();
+System.Console.Clear();
+
+do
+{
+
+
+    // Imprimir guía de coeficientes de equis
+    for (int i = 0; i <= orden; i++)
+    {
+        System.Console.Write("x^{0}  ", orden - i);
+    }
+    // Imprimir coeficientes
+    System.Console.WriteLine("\n");
+    for (int i = 0; i <= orden; i++)
+    {
+        System.Console.Write("{0}    ", poly[i]);
+    }
+    System.Console.WriteLine("\n----------------------------");
+
+    for (int i = 0; i < orden; i++)
+    {
+        System.Console.Write("{0}    ", poly[i] * equis + poly[i + 1]);
+    }
+
+
+    System.Console.ReadKey();
+    System.Console.Clear();
+} while (continuar == true);
